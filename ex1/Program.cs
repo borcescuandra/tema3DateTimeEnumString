1f6cd833ae4a8a25e82fa5e733e13ff5e16874d8
borcescuandra <borcescuandra@users.noreply.github.com>
2019-12-06 21:01:08 +0200

    Add project files.

---
+++ ex1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex22
+{
+    class Program
+    {
+        public enum Gender
+        {
+            Male = 'M',
+            Female = 'F',
+            Invalid
+        }
+
+        static string GetGender(int gender)
+        {
+            switch (gender)
+            {
+                case 'M':
+                    return "Male -> retirement date: 65";
+
+                case 'F':
+                    return "Female -> retirement date: 63";
+
+                default:
+                    return null;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti anul:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti luna:");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti ziua:");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduceti genul(M/F):");
+            char gender = Convert.ToChar(Console.ReadLine());
+
+            DateTime birthDate = new DateTime(year, month, day);
+            Console.WriteLine(birthDate.ToString("dd/MM/yyyy"));
+
+            DateTime now = DateTime.Now;
+
+            int age = now.Year - birthDate.Year;
+
+            Console.WriteLine("Varsta = {0}", age);
+            Gender? Invalid = null;
+
+            Console.WriteLine("Gender = {0}", GetGender(gender));
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ex22/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str1 = "";
+            String str2 = "Hello world";
+            String str3 = "myStRing";
+            String str4 = "always";
+            String str5 = "I like coding.";
+            String str6 = "   newString     ";
+            String str7 = null;
+            String str8 = ", user!";
+            String str9 = "llo";
+
+            bool test1 = String.IsNullOrEmpty(str1);
+            bool test2 = String.IsNullOrEmpty(str7);
+
+
+
+            Console.WriteLine(test1);
+            Console.WriteLine(test2);
+
+            Console.WriteLine(String.Compare(str2, str3));
+            Console.WriteLine(str6);
+            Console.WriteLine(str6.Trim());
+            Console.WriteLine(str3.ToLower());
+            Console.WriteLine(str4.ToUpper());
+
+            String newString = String.Concat(str2, str8);
+            Console.WriteLine(newString);
+
+            Console.WriteLine(str4.IndexOf("ways"));
+            Console.WriteLine(str2.Contains(str9));
+
+        }
+    }
+}
